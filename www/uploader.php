1da1fd10d24b79614e70566602726c993b6aeeb0
freejoe76 <jmurphy@denverpost.com>
2015-07-17 18:04:30 -0400

    Add style, start on back-end

--- www/uploader.php
+++ www/uploader.php
@@ -4,11 +4,11 @@ ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(-1);
 
-if(isset($_FILES["image"])) {
+if(isset($_FILES["audio"])) {
     echo "<div id='message'>";
-    if ($_FILES["image"]["error"] > 0) {
-        if ($_FILES["image"]["error"]==4) { echo "<div style='background-color:red'>No Image was chosen to be uploaded</div>"; exit; } // No image file was uploaded
-        else { echo "<div style='background-color:red'>Error Code: " . $_FILES["image"]["error"] . "</div>"; exit; } // Another error occurred
+    if ($_FILES["audio"]["error"] > 0) {
+        if ($_FILES["audio"]["error"]==4) { echo "<div style='background-color:red'>No Image was chosen to be uploaded</div>"; exit; } // No image file was uploaded
+        else { echo "<div style='background-color:red'>Error Code: " . $_FILES["audio"]["error"] . "</div>"; exit; } // Another error occurred
         }
     else {
         require('constants.php');
@@ -20,30 +20,30 @@ if(isset($_FILES["image"])) {
         $day      = $selectedDate[1];
         $year     = $selectedDate[2];
         $location = "/".$year."/".$month."/";                 // location the file will be put into
-        $_FILES["image"]["name"] = $year."-".$month."-".$day; // rename the file to today's date
+        $_FILES["audio"]["name"] = $year."-".$month."-".$day; // rename the file to today's date
 
         if (!file_exists($FTP_DIRECTORY."/".$year."/"))            { ftp_mkdir($conn_id, $FTP_DIRECTORY."/".$year); }            // if the year folder does not exist, create it
         if (!file_exists($FTP_DIRECTORY."/".$year."/".$month."/")) { ftp_mkdir($conn_id, $FTP_DIRECTORY."/".$year."/".$month); } // if the month folder does not exist, create it
 
-        if (($_FILES["image"]["type"]=="application/pdf")||($_FILES["image"]["type"]=="image/jpeg")) {
+        if (($_FILES["audio"]["type"]=="application/pdf")||($_FILES["audio"]["type"]=="image/jpeg")) {
             $extension = ".pdf";
-            if($_FILES["image"]["type"]=="image/jpeg") { $extension = "-original.jpg"; }
-            move_uploaded_file($_FILES["image"]["tmp_name"], $_FILES["image"]["name"].$extension); // drop original file in current folder for imagick to use
+            if($_FILES["audio"]["type"]=="image/jpeg") { $extension = "-original.jpg"; }
+            move_uploaded_file($_FILES["audio"]["tmp_name"], $_FILES["audio"]["name"].$extension); // drop original file in current folder for imagick to use
             $im = new imagick();
             $im->setResolution(72,72);
-            $im->readimage($_FILES["image"]["name"].$extension);
+            $im->readimage($_FILES["audio"]["name"].$extension);
             $im->setImageFormat('jpg');
             $im->scaleImage(350,0);
-            $im->writeImage($_FILES["image"]["name"].".jpg"); // Create the smaller jpg version in current folder
+            $im->writeImage($_FILES["audio"]["name"].".jpg"); // Create the smaller jpg version in current folder
             $im->clear();
 
-            if (ftp_put($conn_id, $FTP_DIRECTORY.$location.$_FILES["image"]["name"].$extension, $_FILES["image"]["name"].$extension, FTP_BINARY)) {
+            if (ftp_put($conn_id, $FTP_DIRECTORY.$location.$_FILES["audio"]["name"].$extension, $_FILES["audio"]["name"].$extension, FTP_BINARY)) {
                 echo "<div style='background-color:green'>Original file uploaded!</div>";
                 }
             else { echo "<div style='background-color:red'><span style='font-weight:bold'>ERROR</span> :: The original file did not upload!</div>"; }
-            if (ftp_put($conn_id, $FTP_DIRECTORY.$location.$_FILES["image"]["name"].".jpg", $_FILES["image"]["name"].".jpg", FTP_BINARY)) {
+            if (ftp_put($conn_id, $FTP_DIRECTORY.$location.$_FILES["audio"]["name"].".jpg", $_FILES["audio"]["name"].".jpg", FTP_BINARY)) {
 
-                $filename = '"'.$_FILES["image"]["name"].'"';
+                $filename = '"'.$_FILES["audio"]["name"].'"';
                 $json_a = explode("[", file_get_contents("frontpages.json"));
                 $json_b = explode("]", $json_a[1]);
                 $json_c = explode(",", $json_b[0]);
@@ -83,11 +83,11 @@ if(isset($_FILES["image"])) {
                 }
             else { echo "<div style='background-color:red'><span style='font-weight:bold'>ERROR</span> :: The .jpg file did not upload!</div>"; }
 
-            unlink($_FILES["image"]["name"].".jpg");     // delete the jpg file in current folder
-            unlink($_FILES["image"]["name"].$extension); // delete the pdf file in current folder
+            unlink($_FILES["audio"]["name"].".jpg");     // delete the jpg file in current folder
+            unlink($_FILES["audio"]["name"].$extension); // delete the pdf file in current folder
             }
 
-        else { echo "<div style='background-color:red'>File must be a pdf or jpg file!<br> This file is: ".$_FILES["image"]["type"]."</div>"; }
+        else { echo "<div style='background-color:red'>File must be a pdf or jpg file!<br> This file is: ".$_FILES["audio"]["type"]."</div>"; }
         echo "</div>";
         ftp_close($conn_id);
     }
@@ -104,6 +104,7 @@ if(isset($_FILES["image"])) {
 #message               { position: absolute; margin-top: 250px; }
 #message div           { padding: 0px 10px; }
 </style>
+<link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="css/jquery-ui.css">
 <script src="js/jquery-latest.min.js"></script>
 <script src="js/jquery-ui.js"></script>
@@ -129,5 +130,5 @@ function populateInput() {
     <p id="audio">
         <label for="audio">Audio File:</label> <input name="audio" type="file" />
     </p>
-   <input type="submit" name="submit" value="upload the File" onclick="populateInput();">
+   <input type="submit" name="submit" value="Upload">
 </form>
